dd743c6b48fcd11418cab4a7634e98fc22948316
Star305 <emmanuelokeh34@gmail.com>
2025-07-23 23:36:39 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,21 @@
-
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID
+  apiKey: "AIzaSyDl_PMesLgsj0dyJgn02xTbrCoM3edQx80",
+  authDomain: "star-app-c772b.firebaseapp.com",
+  projectId: "star-app-c772b",
+  storageBucket: "star-app-c772b.firebasestorage.app",
+  messagingSenderId: "318197762056",
+  appId: "1:318197762056:web:e4e298f61e4cd079fee9bd",
+  measurementId: "G-Q6HRJ2SYLH"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
-export const provider = new GoogleAuthProvider();
+const analytics = getAnalytics(app);
